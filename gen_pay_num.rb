6cd1776753ad21125771a9dbfe66fd095c87a72a
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-08-16 17:34:19 +0000

    parse csv file to an array

--- gen_pay_num.rb
+++ gen_pay_num.rb
@@ -1,30 +1,47 @@
 
-def generate(digit_needed, x)
-    # digit_needed is the total digits that are required to be generated, ex generate 10 digit this argument will be 10
-    # x is length of repeating numbers which is not allowed in generated digits
-    last_digit = -1
-    same_length = 0
-    # generate first random digit
-    result = ""
-    next_digit = rand(10)
-    while (result.size() < digit_needed) do 
+# def generate(digit_needed, x)
+#     # digit_needed is the total digits that are required to be generated, ex generate 10 digit this argument will be 10
+#     # x is length of repeating numbers which is not allowed in generated digits
+#     last_digit = -1
+#     same_length = 1
+#     # generate first random digit
+#     result = ""
+#     next_digit = rand(10)
+#     while (result.size() < digit_needed) do 
       
-      if next_digit != last_digit 
-         result.concat(next_digit.to_s)
-         last_digit = next_digit
-         same_length = 0  
-      elsif same_length + 2 < x 
-         result.concat(next_digit.to_s)
-         same_length += 1
+#       if next_digit != last_digit 
+#          result.concat(next_digit.to_s)
+#          last_digit = next_digit
+#          same_length = 1  
+#       elsif same_length + 1 < x 
+#          result.concat(next_digit.to_s)
+#          same_length += 1
         
-      end 
+#       end 
      
-      next_digit = rand(10)    
+#       next_digit = rand(10)    
 
-    end
-    return result
+#     end
+#     return result
     
+# end
+
+
+# puts generate(6,3)
+
+require 'csv'
+
+filename = File.dirname(File.dirname(File.expand_path(__FILE__))) + '/payment-number/test_demo.csv'
+exist_numbers = []
+CSV.foreach(filename) do |row|
+  exist_numbers << row[0]
 end
 
+puts exist_numbers[2]
 
-puts generate(5,3)
+
+# CSV.open('/payment-number/test_demo.csv', 'r', ';') do |row|
+#   puts row
+# end
+
+# https://snippets.aktagon.com/snippets/246-how-to-parse-csv-data-with-ruby
